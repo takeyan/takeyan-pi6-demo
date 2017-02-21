dd2300af0599bffaee622440ae3da48ce939a706
Seiichi Takeuchi <takeyan.g@gmail.com>
2017-02-21 07:24:28 +0000

    authorization changed

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var AWS = require('aws-sdk'); 
+AWS.config.update({
+	accessKeyId: 'J8aPAKcK9TafeYO9YhVE',
+	secretAccessKey: 'opDKChX0NwT5AvQkS9lbOgjiKhaReYgmiUuhAgr3',
+});
 var s3 = new AWS.S3({endpoint: 's3-api.us-geo.objectstorage.softlayer.net'} ); 
 var parms = { Bucket: 'takeyan'} ;
 s3.listObjects(parms, function (err, data) {
