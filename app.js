50141fbf40d8f89e9e16e635bd4eec1a7a6cac09
Seiichi Takeuchi <takeyan.g@gmail.com>
2017-02-21 08:43:45 +0000

    express implemented

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var AWS = require('aws-sdk'); 
+var express = require('express');
+app = express();
 AWS.config.update({
 	accessKeyId: 'J8aPAKcK9TafeYO9YhVE',
 	secretAccessKey: 'opDKChX0NwT5AvQkS9lbOgjiKhaReYgmiUuhAgr3',
 });
 var s3 = new AWS.S3({endpoint: 's3-api.us-geo.objectstorage.softlayer.net'} ); 
 var parms = { Bucket: 'takeyan'} ;
-s3.listObjects(parms, function (err, data) {
- if(err)throw err;
- console.log(data);
+var st;
+
+app.get('/', function(req, res) {
+
+ s3.listObjects(parms, function (err, data) {
+  if(err){
+      st = err;
+      console.log("### ERROR:" + st);
+  }
+  st = data;
+  console.log(st);
  });
+ 
+ res.send('### Response from ICOS:' + JSON.stringify(st));
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('listening at:', port);
