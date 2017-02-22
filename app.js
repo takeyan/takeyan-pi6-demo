c654f5e7465a10209ed514f356cb74094a5fa862
Seiichi Takeuchi <takeyan.g@gmail.com>
2017-02-22 13:28:44 +0900

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-var AWS = require('aws-sdk'); 
+var watson = require('watson-developer-cloud');
 var express = require('express');
 var app = express();
-AWS.config.update({
-	accessKeyId: 'J8aPAKcK9TafeYO9YhVE',
-	secretAccessKey: 'opDKChX0NwT5AvQkS9lbOgjiKhaReYgmiUuhAgr3',
-});
-var s3 = new AWS.S3({endpoint: 's3-api.us-geo.objectstorage.softlayer.net'} ); 
-var parms = { Bucket: 'takeyan'} ;
+
+// 安倍首相所信表明抜粋
+var st =  "世界一への執念。";
+ st = st + "歴代最多のメダルラッシュとなったリオ五輪では、世界の強豪たちに真っ向勝負を挑み、最後の一瞬まで勝利を諦めない選手たちの姿に、日本中が感動しました。";
+ st = st + "四年後の東京オリンピック・パラリンピックは、必ずや、世界一の大会にする。何としても、成功させなければなりません。同時に、我が国の「未来」を切り拓く。私たちもまた、世界一暮らしやすい国、世界一信頼される国を目指し、新たなスタートを切る時です。";
+ st = st + "参議院選挙で、自由民主党と公明党の連立与党は、目標の改選過半数を大きく上回る勝利を得ることができました。";
+ st = st + "「この道を、力強く、前へ」";
+ st = st + "これが、選挙で示された国民の意思であります。安定的な政治基盤の上に、しっかりと結果を出していく。国民の負託に応えていく決意であります。";
+ st = st + "この国会に求められていることは、目の前の課題から逃げることではありません。挑戦です。いかに困難な課題にもチャレンジし、建設的な議論を行って「結果」を出すことであります。";
+ st = st + "一億総活躍、地方創生、農政新時代、そして地球儀を俯瞰する外交。安倍内閣は「未来」への挑戦を続けます。世界の真ん中で輝く、日本の「未来」を、皆さん、共に切り拓いていこうではありませんか。";
 
 app.get('/', function(req, res) {
- s3.listObjects(parms, function (err, data) {
-  if(err) console.log("### ERROR:" + err);
-  else {
-      console.log(data);
-      res.send('### Response from ICOS:' + JSON.stringify(data));
-  }
- });
+
+var personality_insights = watson.personality_insights({
+   username: '8e6e7927-3343-491b-9932-81a81835f4a5',
+   password: 'XgTNfJyoEkIt',
+  version: 'v2'
 });
+
+console.log("### Input Text is: " + st);
+
+personality_insights.profile({
+   text: st,
+   content_type:'text/plain',
+   language:'ja',
+   accept_language:'ja'
+   },
+   function (err, response) {
+     if (err)
+       console.log('error:', err);
+     else
+       console.log(JSON.stringify(response, null, 2));
+       res.send('### Response from PI:' + JSON.stringify(response, null, 2));
+ }); 
+ }); 
 
 var port = process.env.PORT || 3000;
 app.listen(port);
-console.log('### listening at:', port);
+console.log("### Start listening on port " + port);
